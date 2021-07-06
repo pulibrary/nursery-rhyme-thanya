ed38dd97b7bfbb08c82ed4a8580395a23b7f9145
Thanya Begum <tbegum@princeton.edu>
2021-07-06 15:41:46 -0400

    Implement Rhyme class

--- lib/rhyme.rb
+++ lib/rhyme.rb
@@ -1,3 +1,36 @@
 class Rhyme
+  def initialize
+    @blanks = []
+    add_all_blanks
+  end
 
+  def add_blank(subject, verb)
+    blank = {"subject": subject, "verb": verb}
+    @blanks.push(blank)
+  end
+
+  def add_all_blanks
+    add_blank("house", "Jack built")
+    add_blank("malt", "lay in")
+    add_blank("rat", "ate")
+    add_blank("cat", "killed")
+    add_blank("dog", "worried")
+    add_blank("cow with the crumpled horn", "tossed")
+    add_blank("maiden all forlorn", "milked")
+    add_blank("man all tattered and torn", "kissed")
+    add_blank("priest all shaven and shorn", "married")
+    add_blank("rooster that crowed in the morn", "woke")
+    add_blank("farmer sowing his corn", "kept")
+    add_blank("horse and the hound and the horn", "belonged to")
+  end
+
+  def to_s
+    rhyme = ""
+    clause = "."
+    @blanks.each do |blank|
+      clause.prepend(" the #{blank[:subject]} that #{blank[:verb]}")
+      rhyme << "This is" + clause + "\n"
+    end
+    return rhyme
+  end
 end
