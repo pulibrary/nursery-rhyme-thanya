d27b054ca34c15f4877c52c12f024096abcfcfbb
Thanya Begum <tbegum@princeton.edu>
2021-07-07 13:33:03 -0400

    Refactor rhyme.rb

--- lib/rhyme.rb
+++ lib/rhyme.rb
@@ -1,34 +1,26 @@
 class Rhyme
   def initialize
-    @blanks = []
-    add_all_blanks
-  end
-
-  def add_blank(subject, action)
-    blank = {"subject": subject, "action": action}
-    @blanks.push(blank)
-  end
-
-  def add_all_blanks
-    add_blank("house", "Jack built")
-    add_blank("malt", "lay in")
-    add_blank("rat", "ate")
-    add_blank("cat", "killed")
-    add_blank("dog", "worried")
-    add_blank("cow with the crumpled horn", "tossed")
-    add_blank("maiden all forlorn", "milked")
-    add_blank("man all tattered and torn", "kissed")
-    add_blank("priest all shaven and shorn", "married")
-    add_blank("rooster that crowed in the morn", "woke")
-    add_blank("farmer sowing his corn", "kept")
-    add_blank("horse and the hound and the horn", "belonged to")
+    @sentences = [
+      "the house that Jack built.",
+      "the malt that lay in",
+      "the rat that ate",
+      "the cat that killed",
+      "the dog that worried",
+      "the cow with the crumpled horn that tossed",
+      "the maiden all forlorn that milked",
+      "the man all tattered and torn that kissed",
+      "the priest all shaven and shorn that married",
+      "the rooster that crowed in the morn that woke",
+      "the farmer sowing his corn that kept",
+      "the horse and the hound and the horn that belonged to"
+    ] 
   end
 
   def to_s
     rhyme = ""
-    clause = "."
-    @blanks.each do |blank|
-      clause.prepend(" the #{blank[:subject]} that #{blank[:action]}")
+    clause = ""
+    @sentences.each do |sentence|
+      clause.prepend(" " + sentence)
       rhyme << "This is" + clause + "\n"
     end
     return rhyme
