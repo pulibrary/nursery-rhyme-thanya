03045a5965c7e66f00d2f64120ad1f4ca8cba836
Thanya Begum <tbegum@princeton.edu>
2021-07-08 15:43:16 -0400

    Rename variables

--- spec/rhyme_spec.rb
+++ spec/rhyme_spec.rb
@@ -13,8 +13,8 @@ describe Rhyme do
 
     it "returns the nursery rhyme in a random order" do
       rhyme = Rhyme.new(:random)
-      shuffled_clauses = [8, 6, 11, 0, 5, 7, 1, 9, 10, 4, 3, 2].map do |clause_index|
-        Rhyme::CLAUSES[clause_index]
+      shuffled_clauses = [8, 6, 11, 0, 5, 7, 1, 9, 10, 4, 3, 2].map do |index|
+        Rhyme::CLAUSES[index]
       end
       allow_any_instance_of(Array).to receive(:shuffle).and_return(shuffled_clauses)
       expect(rhyme.recite).to match(random_rhyme_file)
@@ -22,10 +22,10 @@ describe Rhyme do
 
     it "returns the nursery rhyme in a semi-random order" do
       rhyme = Rhyme.new(:semi_random)
-      order = [4, 9, 8, 2, 5, 10, 1, 3, 7, 11, 6].map do |clause_index|
-        Rhyme::CLAUSES[clause_index]
+      shuffled_clauses = [4, 9, 8, 2, 5, 10, 1, 3, 7, 11, 6].map do |index|
+        Rhyme::CLAUSES[index]
       end
-      allow_any_instance_of(Array).to receive(:shuffle).and_return(order)
+      allow_any_instance_of(Array).to receive(:shuffle).and_return(shuffled_clauses)
       expect(rhyme.recite).to match(semi_random_rhyme_file)
     end
   end
